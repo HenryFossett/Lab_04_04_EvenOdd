a862095e5fb5fd945b28913f209433de7acdbab8
Henry Fossett <henry.fossett@gmail.com>
2023-09-17 17:55:52 -0400

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main (String[] args){
+        int userNum = 7;
+        int evenOrOdd = 0;
+        evenOrOdd = userNum % 2;
+        System.out.println("If the remainder is 1, your number is odd. If the remainder is 0, your number is even. The remainder of " + userNum + " is " + evenOrOdd);
+    }
+}
